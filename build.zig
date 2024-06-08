979205922e20c0f206e61c56ff4e48fe63910ead
Daethalus <daethalus@gmail.com>
2024-06-08 10:04:21 +0100

    starting with windows

--- build.zig
+++ build.zig
@@ -1,91 +1,113 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+const FileArray = std.ArrayList([]const u8);
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
+fn addSourceFiles(b: *std.Build, sources: *FileArray, sub_path: []const u8) !void {
+    var dir = try std.fs.cwd().openDir(sub_path, .{ .iterate = true });
+    var iter = dir.iterate();
+    while (try iter.next()) |file| {
+        if (file.kind != .file) {
+            continue;
+        }
+
+        const ext = std.fs.path.extension(file.name);
+        if (std.mem.eql(u8, ext, ".c")) {
+            const paths = [_][]const u8{ sub_path, file.name };
+            const full_path = b.pathJoin(&paths);
+            try sources.append(full_path);
+        }
+    }
+}
+
+pub fn build(b: *std.Build) !void {
+    var sources = FileArray.init(b.allocator);
+    defer sources.deinit();
+
+    try addSourceFiles(b, &sources, "src");
+    try addSourceFiles(b, &sources, "src/atomic");
+    try addSourceFiles(b, &sources, "src/audio");
+    try addSourceFiles(b, &sources, "src/camera");
+    try addSourceFiles(b, &sources, "src/core");
+    try addSourceFiles(b, &sources, "src/cpuinfo");
+    try addSourceFiles(b, &sources, "src/dynapi");
+    try addSourceFiles(b, &sources, "src/events");
+    try addSourceFiles(b, &sources, "src/file");
+    try addSourceFiles(b, &sources, "src/filesystem");
+    try addSourceFiles(b, &sources, "src/joystick");
+    try addSourceFiles(b, &sources, "src/haptic");
+    try addSourceFiles(b, &sources, "src/hidapi");
+    try addSourceFiles(b, &sources, "src/libm");
+    try addSourceFiles(b, &sources, "src/locale");
+    try addSourceFiles(b, &sources, "src/main");
+    try addSourceFiles(b, &sources, "src/misc");
+    try addSourceFiles(b, &sources, "src/power");
+    try addSourceFiles(b, &sources, "src/render");
+    try addSourceFiles(b, &sources, "src/render");
+    try addSourceFiles(b, &sources, "src/sensor");
+    try addSourceFiles(b, &sources, "src/stdlib");
+    try addSourceFiles(b, &sources, "src/storage");
+    try addSourceFiles(b, &sources, "src/thread");
+    try addSourceFiles(b, &sources, "src/time");
+    try addSourceFiles(b, &sources, "src/timer");
+    try addSourceFiles(b, &sources, "src/video");
+
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const t = target.result;
+
     const lib = b.addStaticLibrary(.{
-        .name = "SDL",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
+        .name = "SDL3",
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    lib.addIncludePath(b.path("include"));
+    lib.addIncludePath(b.path("src"));
 
-    const exe = b.addExecutable(.{
-        .name = "SDL",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    lib.defineCMacro("SDL_USE_BUILTIN_OPENGL_DEFINITIONS", "1");
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+    lib.linkLibC();
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+    if (t.os.tag.isDarwin()) {
+        lib.linkFramework("OpenGL");
+        lib.linkFramework("Metal");
+        lib.linkFramework("CoreVideo");
+        lib.linkFramework("Cocoa");
+        lib.linkFramework("IOKit");
+        lib.linkFramework("ForceFeedback");
+        lib.linkFramework("Carbon");
+        lib.linkFramework("CoreAudio");
+        lib.linkFramework("AudioToolbox");
+        lib.linkFramework("AVFoundation");
+        lib.linkFramework("Foundation");
+    } else if (t.os.tag == .windows) {
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+        try addSourceFiles(b, &sources, "src/core/windows");
+        try addSourceFiles(b, &sources, "src/misc/windows");
+        try addSourceFiles(b, &sources, "src/audio/directsound");
+        try addSourceFiles(b, &sources, "src/audio/wasapi");
+        try addSourceFiles(b, &sources, "src/video/windows/");
+        try addSourceFiles(b, &sources, "src/locale/windows");
+        try addSourceFiles(b, &sources, "src/filesystem/windows");
+        try addSourceFiles(b, &sources, "src/timer/windows");
+        try addSourceFiles(b, &sources, "src/time/windows");
+        try addSourceFiles(b, &sources, "src/storage/steam");
+        try addSourceFiles(b, &sources, "src/storage/generic");
+        try addSourceFiles(b, &sources, "src/joystick/windows");
+        try addSourceFiles(b, &sources, "src/loadso/windows");
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        lib.linkSystemLibrary("setupapi");
+        lib.linkSystemLibrary("winmm");
+        lib.linkSystemLibrary("gdi32");
+        lib.linkSystemLibrary("imm32");
+        lib.linkSystemLibrary("version");
+        lib.linkSystemLibrary("oleaut32");
+        lib.linkSystemLibrary("ole32");
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    lib.addCSourceFiles(.{ .files = sources.items });
+    lib.installHeadersDirectory(b.path("include"), "SDL3", .{});
+    b.installArtifact(lib);
 }
